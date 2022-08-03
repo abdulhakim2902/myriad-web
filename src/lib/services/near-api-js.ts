7ad1978d0e352409ecb89d1e597f9ca1072419dc
abdulhakim2902 <hakim@blocksphere.id>
2022-08-03 16:34:38 +0700

    feat: send amount v2

--- src/lib/services/near-api-js.ts
+++ src/lib/services/near-api-js.ts
@@ -389,6 +389,68 @@ export class Near implements IProvider {
     return { partialFee: BN_ZERO };
   }
 
+  async sendAmountV2(receiver: string, amount: BN, referenceId?: string): Promise<void> {
+    const wallet = this.provider.wallet;
+
+    if (referenceId) {
+      const ONE_YOCTO = '1';
+      const MAX_GAS = '300000000000000';
+      const ATTACHED_AMOUNT = '1250000000000000000000';
+      const ATTACHED_GAS = '10000000000000';
+      const contract = await this.contractInitialize(referenceId);
+      const check_storage = await (contract as any).storage_balance_of({account_id: receiver});
+      const actions: nearAPI.transactions.Action[] = [];
+
+      if (!check_storage) {
+        actions.push(
+          nearAPI.transactions.functionCall(
+            'storage_deposit',
+            Buffer.from(JSON.stringify({account_id: receiver, registration_only: true})),
+            new BN(ATTACHED_GAS),
+            new BN(ATTACHED_AMOUNT),
+          ),
+        );
+      }
+
+      actions.push(
+        nearAPI.transactions.functionCall(
+          'ft_transfer',
+          Buffer.from(JSON.stringify({receiver_id: receiver, amount: amount.toString()})),
+          new BN(MAX_GAS).sub(new BN(ATTACHED_GAS)),
+          new BN(ONE_YOCTO),
+        ),
+      );
+
+      const accountId = wallet.getAccountId();
+      const provider = new nearAPI.providers.JsonRpcProvider(
+        `https://rpc.${wallet._networkId}.near.org`,
+      );
+      const {
+        public_key,
+        access_key: {nonce},
+      } = await wallet.account().accessKeyForTransaction(referenceId, actions);
+      const accessKey = await provider.query(
+        `access_key/${accountId}/${public_key.toString()}`,
+        '',
+      );
+      const recentBlockHash = nearAPI.utils.serialize.base_decode(accessKey.block_hash);
+      const signer = new nearAPI.InMemorySigner(wallet._keyStore);
+      const publicKey = await signer.getPublicKey(accountId, wallet._networkId);
+      const transaction = nearAPI.transactions.createTransaction(
+        accountId,
+        publicKey,
+        referenceId,
+        nonce + 1,
+        actions,
+        recentBlockHash,
+      );
+      await wallet.requestSignTransactions({transactions: [transaction]});
+    } else {
+      const account = wallet.account();
+      await account.sendMoney(receiver, amount);
+    }
+  }
+
   private async contractInitialize(
     contractId: string,
     viewMethod?: string,
